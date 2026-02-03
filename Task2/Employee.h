f14c2b48a8846c549322ce27d0555282048cb885
Abbas Ali Haider <abbasaali5152@gmail.com>
2026-02-03 22:09:14 +0500

    Update Employee.h

--- Task2/Employee.h
+++ Task2/Employee.h
@@ -1,28 +1,19 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
+#include <iostream>
 #include <string>
 using namespace std;
 
-class Employee {
-private:
+class Employee{
     string name;
-    int id;
     double salary;
+    double taxPercentage;
 
 public:
-    Employee();                      
-    Employee(string n, int i, double s);
-
-    void setName(string n);
-    void setId(int i);
-    void setSalary(double s);
-
-    string getName();
-    int getId();
-    double getSalary();
-
-    void display();
+    void get_Data();
+    void Salary_after_tax();
+    void update_tax_percentage();
 };
 
 #endif
