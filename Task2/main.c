a2879262f948f6c320d1184d388a6d7ea142204e
Abbas Ali Haider <abbasaali5152@gmail.com>
2026-02-03 22:15:09 +0500

    Update main.c

--- Task2/main.c
+++ Task2/main.c
@@ -1,15 +1,22 @@
-#include <iostream>
 #include "Employee.h"
-using namespace std;
 
-int main() {
-    Employee e1("Ali", 101, 50000);
-    Employee e2("Sara", 102, 60000);
-    Employee e3("Ahmed", 103, 55000);
+int main(){
+    Employee emp1, emp2, emp3;
 
-    e1.display();
-    e2.display();
-    e3.display();
+    cout << "Employee 1:\n";
+    emp1.get_Data();
+    emp1.Salary_after_tax();
+    emp1.update_tax_percentage();
+
+    cout << "\nEmployee 2:\n";
+    emp2.get_Data();
+    emp2.Salary_after_tax();
+    emp2.update_tax_percentage();
+
+    cout << "\nEmployee 3:\n";
+    emp3.get_Data();
+    emp3.Salary_after_tax();
+    emp3.update_tax_percentage();
 
     return 0;
 }
